bd40cb2b11bdf43ffb72947b50223adc322315a4
Gustavo Ramos <gu.gugovisk1@gmail.com>
2023-05-20 18:59:03 -0300

    Cria método initialize em classe Customer

--- classes/customer.rb
+++ classes/customer.rb
@@ -1,5 +1,10 @@
 class Customer
     @@no_of_customers = 0
+    def initialize(id, name, addr)
+        @cust_id = id
+        @cust_name = name
+        @cust_addr = addr
+     end
  end
 
 cust1 = Customer.new
