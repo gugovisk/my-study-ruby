1caa0b50b1a59a95fa2058b70307351717683896
Gustavo Ramos <gu.gugovisk1@gmail.com>
2023-05-21 23:18:13 -0300

    Class Language: Modifying attributes

--- classes/language.rb
+++ classes/language.rb
@@ -1,40 +1,21 @@
 # Ruby program to understand the concept of 
-# Class Variables
+# Modifying attributes
 class Language
-    $reader = 'ABCD'
-  
-    # Creating class variable
-    @@count = 0
-    def initialize(language_name, topic_name)
+    attr_reader :language_name
+    attr_writer :topic_name
+    attr_reader :topic_name
+
+def initialize(language_name, topic_name)
         @language_name = language_name
         @topic_name = topic_name
-        @@count  += 1
-    end
-  
-    # Defining method
-    def return_name
-        @language_name
-    end
-    def return_topic
-        @topic_name
-    end
-      
-    # Returning class variable
-    def  return_count
-        @@count
-    end
 end
   
-# Creating object
-object1 = Language.new('Ruby', 'method') 
-object2 = Language.new('Scala', 'string')
-puts 'Language name for object1: '+object1.return_name
-puts 'Topic name for object1: '+object1.return_topic
+end
   
-puts 'Language name for object2: '+object2.return_name
-puts 'Topic name for object2: '+object2.return_topic
-  
-puts 'The reader is '+ $reader 
-  
-puts 'The number of objects created is ' + object1.return_count.to_s
-puts 'The number of objects created is ' + object2.return_count.to_s
+object = Language.new('Ruby', 'method') 
+puts 'The name of the language is ' + object.language_name
+puts 'The topic of the language is ' + object.topic_name
+
+# changing the topic name
+object.topic_name = 'array'
+puts 'The new topic of the language is ' + object.topic_name
